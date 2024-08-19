e1a2f078794f07c57893c6c8795da36e01fcf8b1
Beatriz Maia <149596621+beatrizmaiar@users.noreply.github.com>
2024-08-18 22:26:52 -0300

    criando modelo de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,43 @@
 class User < ApplicationRecord
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }, if: :password_required?
+  validate :password_complexity, if: :password_required?
 
   has_secure_password
+
+
+  def authenticate(password)
+    super(password) 
+  end
+
+  def admin?
+    self.role == 'admin'
+  end
+
+  def generate_auth_token
+    self.auth_token = SecureRandom.hex(10)
+    save
+    self.auth_token
+  end
+
+  def reset_password(new_password)
+    self.password = new_password
+    save
+  end
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    unless password.match(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/)
+      errors.add :password, 'deve conter pelo menos uma letra e um dígito, e ter no mínimo 8 caracteres'
+    end
+  end
+
+  def password_required?
+    password_digest.nil? || !password.nil?
+  end
 end
